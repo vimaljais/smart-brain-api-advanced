937da46152bea715cc1391b1335a9edc69e6554e
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2021-03-25 17:58:15 +0530

    docker added

--- controllers/image.js
+++ controllers/image.js
@@ -1,8 +1,8 @@
-const Clarifai = require('clarifai');
+const Clarifai = require("clarifai");
 
-//You must add your own API key here from Clarifai. 
+//You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR API KEY HERE'
+  apiKey: "fec298c2589743d0b0b810ce8b9d5336",
 });
 
 const handleApiCall = (req, res) => {
@@ -17,24 +17,25 @@ const handleApiCall = (req, res) => {
     // to:
     // .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
+    .then((data) => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API'))
-}
+    .catch((err) => res.status(400).json("unable to work with API"));
+};
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-}
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+};
 
 module.exports = {
   handleImage,
-  handleApiCall
-}
+  handleApiCall,
+};
